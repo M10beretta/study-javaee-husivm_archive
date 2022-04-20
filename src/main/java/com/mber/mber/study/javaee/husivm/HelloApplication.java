0967e9f50a115b62f33c9414b25c366492fc94bb
M10beretta <Mberetta.gm@gmail.com>
2022-04-20 22:58:52 +0300

    commit

---
+++ src/main/java/com/mber/mber/study/javaee/husivm/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.mber.mber.study.javaee.husivm;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
