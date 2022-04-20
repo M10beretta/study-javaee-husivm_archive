218b5be067cf3117a4541f95f6a02b68be293431
M10beretta <Mberetta.gm@gmail.com>
2022-04-20 23:36:17 +0300

    commit

--- src/main/java/com/mber/study/javaee/husivm/_9_ConversationScope/Servlet.java
+++ src/main/java/com/mber/study/javaee/husivm/_9_ConversationScope/Servlet.java
@@ -16,33 +16,43 @@ public class Servlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        System.out.println(conversationBean.i);
-        conversationBean.i = 1;
+        System.out.println(conversationBean.state + " : out");   //    null : out       -1
+        conversationBean.state = "1 (out)";
         conversationBean.startConversation();
-        System.out.println(conversationBean.i);
-        System.out.println("middle conversation");
-        conversationBean.i = 3;
+        System.out.println(conversationBean.state + " : out");   // 1 (out) : out       -4
+        System.out.println("middle conversation");               // middle conversation -5
+        conversationBean.doJob();
+        conversationBean.state = "2 (out)";
         conversationBean.endConversation();
-        System.out.println(conversationBean.i);
+        System.out.println(conversationBean.state + " : out");   // 2 (out) : out       -9
+        conversationBean.doJob();
+
     }
 }
 
 @ConversationScoped
 class ConversationBean implements Serializable {
-    int i;
+    String state;
 
     @Inject
     Conversation conversation;
 
     public void startConversation() {
-        System.out.println(i);
-        System.out.println("start conversation");
+        System.out.println(state + " : in");                      //    null : in        -2
+        System.out.println("start conversation");                 // start conversation  -3
         conversation.begin();
-        i = 2;
+        state = "2 (in)";
     }
 
+
+    public void doJob(){
+        System.out.println(state + " : in");                     //    2 (in) : in       -6, 10
+    }
+
+
     public void endConversation() {
-        System.out.println("end conversation");
+        System.out.println(state + " : in");                      //   2 (in) : in       -7
+        System.out.println("end conversation");                   // end conversation    -8
         conversation.end();
     }
 }
