e24a75e9eb65e47ea4c7af1b2ba95fe0768855ce
M10beretta <Mberetta.gm@gmail.com>
2022-05-02 14:38:38 +0300

    commit

---
+++ src/main/java/com/mber/study/javaee/husivm/_005_WebServlet/Servlet.java
@@ -0,0 +1,17 @@
+package com.mber.study.javaee.husivm._005_WebServlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("005")
+public class Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        var printWriter = resp.getWriter();
+        printWriter.write("Hello World!");
+    }
+}
