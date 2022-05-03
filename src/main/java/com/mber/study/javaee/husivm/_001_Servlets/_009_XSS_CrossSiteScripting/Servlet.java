7bc500c993e1e7c6c766f258d31286929d551aab
M10beretta <Mberetta.gm@gmail.com>
2022-05-03 22:35:07 +0300

    commit

---
+++ src/main/java/com/mber/study/javaee/husivm/_001_Servlets/_009_XSS_CrossSiteScripting/Servlet.java
@@ -0,0 +1,37 @@
+package com.mber.study.javaee.husivm._001_Servlets._009_XSS_CrossSiteScripting;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+
+@WebServlet("009")
+public class Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var one = req.getParameter("one");
+        one = Objects.isNull(one) ? "" : one.replaceAll("<", "&lt").replaceAll(">", "&gt;");
+
+        resp.getWriter().write(String.format("""
+                <html>
+                <head></head>
+                <body>
+                one = %s
+                <form action='009' method='POST'/>
+                    <input type='textarea' name='one'>
+                    <input type='submit' name='one'/>
+                    </form>
+                </body>
+                </html>
+                """, one));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
