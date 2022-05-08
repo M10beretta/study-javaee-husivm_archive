1babcded46d0ac4bbd4c2b182d22ac963491daee
M10beretta <Mberetta.gm@gmail.com>
2022-05-08 22:24:32 +0300

    commit

---
+++ src/main/java/com/mber/study/javaee/husivm/_001_Servlets/_014_Session/Servlet.java
@@ -0,0 +1,26 @@
+package com.mber.study.javaee.husivm._001_Servlets._014_Session;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("014")
+public class Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var session = req.getSession();
+        var attributeNames = session.getAttributeNames();
+        while (attributeNames.hasMoreElements()){
+            var s = attributeNames.nextElement();
+            System.out.printf("%s = %s%n", s, session.getAttribute(s));
+        }
+        session.setAttribute("one", "two");
+//        session.removeAttribute("one");
+        System.out.printf("MaxInactiveInterval: %s", session.getMaxInactiveInterval());
+        System.out.printf("isNew: %s", session.isNew());
+//        session.invalidate();
+    }
+}
---
+++ src/main/java/com/mber/study/javaee/husivm/_001_Servlets/_015_AsynchronousServlets/Servlet.java
@@ -0,0 +1,23 @@
+package com.mber.study.javaee.husivm._001_Servlets._015_AsynchronousServlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(value = "015", asyncSupported = true)
+public class Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var asyncContext = req.getAsyncContext();
+        asyncContext.start(new Runnable() {
+            @Override
+            public void run() {
+                //wait event
+            }
+        });
+        asyncContext.complete();
+    }
+}
