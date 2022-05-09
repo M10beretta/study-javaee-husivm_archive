5c19c712266140e404994e7721212f7c34aff5e0
M10beretta <Mberetta.gm@gmail.com>
2022-05-09 18:53:15 +0300

    commit

--- src/main/java/com/mber/study/javaee/husivm/_044_CDI/_052_ConversationScope/Servlet.java
+++ src/main/java/com/mber/study/javaee/husivm/_044_CDI/_052_ConversationScope/Servlet.java
@@ -16,15 +16,23 @@ public class Servlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        System.out.println(bean.state + "    : out"); //    null : out       -1
-        bean.state = "1 (out)";
+        System.out.printf("outside, state: %s%n", bean.state);
+        bean.state = "A";
+        System.out.println("outside, set state - A");
         bean.startConversation();
-        System.out.println(bean.state + " : out");    // 1 (out) : out       -4
-        System.out.println("middle conversation");    // middle conversation -5
+        System.out.printf("outside, state: %s%n", bean.state);
         bean.doJob();
-        bean.state = "2 (out)";
+        bean.state = "C";
+        System.out.println("outside, set state - C");
+        bean.doJob();
+        bean.state = "D";
+        System.out.println("outside, set state - D");
+        bean.doJob();
         bean.endConversation();
-        System.out.println(bean.state + " : out");    // 2 (out) : out       -9
+        System.out.printf("outside, state: %s%n", bean.state);
+        bean.doJob();
+        bean.state = "E";
+        System.out.println("outside, set state - E");
         bean.doJob();
     }
 }
@@ -34,22 +42,23 @@ class ConversationBean implements Serializable {
     String state;
 
     @Inject
-    Conversation conversation;
+    private Conversation conversation;
 
     public void startConversation() {
-        System.out.println(state + "    : in");        // null : in           -2
-        System.out.println("start conversation");      // start conversation  -3
+        System.out.printf("inside, start conversation start, state: %s%n", state);
         conversation.begin();
-        state = "2 (in)";
+        System.out.println("inside, conversation.begin()");
+        state = "B";
+        System.out.println("inside, set state - B, start conversation stop");
     }
 
-    public void doJob(){
-        System.out.println(state + "  : in");          // 2 (in) : in         -6, 10
+    public void doJob() {
+        System.out.printf("inside, do job, state: %s%n", state);
     }
 
     public void endConversation() {
-        System.out.println(state + "  : in");          // 2 (in) : in         -7
-        System.out.println("end conversation");        // end conversation    -8
+        System.out.printf("inside, end conversation start, state: %s%n", state);
         conversation.end();
+        System.out.println("inside, conversation.end(), end conversation stop");
     }
 }
