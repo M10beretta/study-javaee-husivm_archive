e6173256e8423e56d599a0f3b01ba04f1f4e49d7
M10beretta <Mberetta.gm@gmail.com>
2022-05-09 17:08:15 +0300

    commit

---
+++ src/main/java/com/mber/study/javaee/husivm/_044_CDI/_050_InjectionPoint/Servlet.java
@@ -0,0 +1,52 @@
+package com.mber.study.javaee.husivm._044_CDI._050_InjectionPoint;
+
+import org.jetbrains.annotations.NotNull;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@WebServlet("050")
+public class Servlet extends HttpServlet {
+    @Inject
+    private Logger log;
+
+    @Override
+    protected void doGet(HttpServletRequest req, @NotNull HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write(log.getName());
+        log.log(Level.INFO, log.getName());
+    }
+}
+
+class MyLogger {
+    @Produces
+    public Logger getLogger(InjectionPoint ip) {
+        return Logger.getLogger(String.format("""
+                        Annotated: %s
+                        Bean: %s
+                        Member: %s
+                        Member→DeclaringClass→Name: %s
+                        Qualifiers: %s
+                        Type: %s
+                        isDelegate: %s
+                        isTransient: %s
+                        """,
+                ip.getAnnotated(),
+                ip.getBean(),
+                ip.getMember(),
+                ip.getMember().getDeclaringClass().getName(),
+                ip.getQualifiers(),
+                ip.getType(),
+                ip.isDelegate(),
+                ip.isTransient()));
+    }
+}
+
